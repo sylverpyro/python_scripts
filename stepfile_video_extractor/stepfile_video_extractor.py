4a85c96df0d5a9dfa8984df535171f912d78f708
sylverpyro <sylverpyro@users.noreply.github.com>
2022-02-09 21:53:21 -0600

    Minor progress on the main iterrator structure

--- stepfile_video_extractor/stepfile_video_extractor.py
+++ stepfile_video_extractor/stepfile_video_extractor.py
@@ -8,10 +8,20 @@ def main():
     collection_scan()
 
 def collection_scan():
-    target_collection='Pump It Up 10th - Exceed'
-    target_folder='A12 - Go - U-Nee'
-    print(f"Scanning collection {target_collection}")
-    songdir_scan(target_collection, target_folder)
+    #with os.scandir(scan_target) as collections:
+    with os.scandir(scan_target) as collections:
+        for collection_dir in collections:
+            #if os.path.isdir(collection_dir): 
+            if collection_dir.is_dir():
+                print(f"Found collection dir: {collection_dir.path}")
+            else:
+                print(f"Skipping non-dir: {collection_dir.path}")
+
+
+    #target_collection='Pump It Up 10th - Exceed'
+    #target_folder='A12 - Go - U-Nee'
+    #print(f"Scanning collection {target_collection}")
+    #songdir_scan(target_collection, target_folder)
 
 def songdir_scan(collection, song_folder):
     full_path=scan_target + collection + song_folder
